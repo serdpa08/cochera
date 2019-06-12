27bd2a50150f1d116222cc4c904d8d4e1199aecf
sergio de pablos <sergioleng18@gmail.com>
2019-06-12 20:28:28 +0200

    ultimas pruebas y diseños

--- docker-entrypoint-initdb.d/nombre.sql
+++ docker-entrypoint-initdb.d/nombre.sql
@@ -1,21 +1,11 @@
-CREATE TABLE usuarios(
-   nombre VARCHAR(255),
-   rol VARCHAR(255),
-   puntos_poder VARCHAR(255),
-   perfil VARCHAR(255),
-   armas VARCHAR(255),
-   promocion VARCHAR(255),
-   habilidades VARCHAR(255),
-   estratagema VARCHAR(255),
-   clavefaccion VARCHAR(255),
-   clave VARCHAR(255),
-
-); --ENGINE=InnoDB CHARACTER SET=utf8;
+CREATE TABLE `usuarios`(
+   `nombre` VARCHAR(255),
+   `apellidos` VARCHAR(255)
+) ENGINE=InnoDB CHARACTER SET=utf8;
 
 INSERT INTO `usuarios`
-    (`nombre`, `rol`,'puntos_poder','perfil','armas','promocion','habilidades','estratagema','clavefaccion','clave')
+    (`nombre`, `apellidos`)
 VALUES
-    ('Gran Inmundicia', 'Cuartel general','10','4','espada venenosa','-','nube de moscas','chaosdaemos','nurgle'),
-    ('Gran Inmundicia', 'Cuartel general','10','4','espada venenosa','-','nube de moscas','chaosdaemos','nurgle'),
-    ('Gran Inmundicia', 'Cuartel general','10','4','espada venenosa','-','nube de moscas','chaosdaemos','nurgle')
+    ('Benito', 'Lopera Perrote'),
+    ('Manuel', 'Jumilla Pandero')
 ;
